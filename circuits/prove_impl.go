7544533c1828cf024064253742e3aee5fea0e93d
archisman1 <archisman@creatoros.co>
2024-08-05 11:55:18 +0530

    [FEAT] Fetch keys from remote

--- circuits/prove_impl.go
+++ circuits/prove_impl.go
@@ -1,57 +1,61 @@
 package circuits
 
 import (
-	"bytes"
-	_ "embed"
-	"encoding/hex"
-	"encoding/json"
-	"fmt"
-	"sync"
-	"unsafe"
+  "bytes"
+  _ "embed"
+  "encoding/hex"
+  "encoding/json"
+  "fmt"
+  "sync"
+  "unsafe"
+  "time"
+  "net/http"
+  "io/ioutil"
 
-	"github.com/consensys/gnark-crypto/ecc"
-	"github.com/consensys/gnark/backend/groth16"
-	"github.com/rs/zerolog"
+  "github.com/consensys/gnark-crypto/ecc"
+  "github.com/consensys/gnark/backend/groth16"
+  "github.com/rs/zerolog"
 )
 
 // #include <stdlib.h>
 import (
-	"C"
+  "C"
 )
 
 type InputParamsCipher struct {
-	Cipher string `json:"cipher"`
+  Cipher string `json:"cipher"`
 }
 
 type InputParamsChaCha struct {
-	Cipher  string    `json:"cipher"`
-	Key     [][]uint8 `json:"key"`
-	Nonce   [][]uint8 `json:"nonce"`
-	Counter []uint8   `json:"counter"`
-	Input   [][]uint8 `json:"input"`
+  Cipher  string    `json:"cipher"`
+  Key     [][]uint8 `json:"key"`
+  Nonce   [][]uint8 `json:"nonce"`
+  Counter []uint8   `json:"counter"`
+  Input   [][]uint8 `json:"input"`
 }
 
 type InputParamsAES struct {
-	Cipher  string  `json:"cipher"`
-	Key     []uint8 `json:"key"`
-	Nonce   []uint8 `json:"nonce"`
-	Counter []uint8 `json:"counter"`
-	Input   []uint8 `json:"input"`
+  Cipher  string  `json:"cipher"`
+  Key     []uint8 `json:"key"`
+  Nonce   []uint8 `json:"nonce"`
+  Counter []uint8 `json:"counter"`
+  Input   []uint8 `json:"input"`
 }
 
 type Proof struct {
-	ProofJson string `json:"proofJson"`
+  ProofJson string `json:"proofJson"`
 }
 
 type OutputParams struct {
-	Proof         Proof `json:"proof"`
-	PublicSignals []int `json:"publicSignals"`
+  Proof         Proof `json:"proof"`
+  PublicSignals []int `json:"publicSignals"`
 }
 
 type ProverParams struct {
-	Prover
-	wg   *sync.WaitGroup
-	Init func()
+  Prover
+  wg   *sync.WaitGroup
+  Init func()
+  isInit bool
 }
 
 var initChaCha sync.WaitGroup
@@ -60,163 +64,197 @@ var initAES256 sync.WaitGroup
 
 var a, b, c bool
 
-//go:embed generated/pk.bits
-var pkChaChaEmbedded []byte
-
-//go:embed generated/pk.aes128
-var pkAES128Embedded []byte
-
-//go:embed generated/pk.aes256
-var pkAES256Embedded []byte
 
 var InitChaChaFunc = sync.OnceFunc(func() {
-	fmt.Println("loading ChaCha20")
-	defer initChaCha.Done()
+    fmt.Println("loading ChaCha20")
+    defer initChaCha.Done()
 
-	pkChaCha := groth16.NewProvingKey(ecc.BN254)
-	_, err := pkChaCha.ReadFrom(bytes.NewBuffer(pkChaChaEmbedded))
-	if err != nil {
-		panic(err)
-	}
-	provers["chacha20"].Prover = &ChaChaProver{
-		r1cs: GetR1CS("chacha20"),
-		pk:   pkChaCha,
-	}
-	a = true
+    pkChaCha := groth16.NewProvingKey(ecc.BN254)
+    pkData, err := fetchKey("pk.bits")
+    if err != nil {
+        fmt.Println("failed to fetch key")
+        panic(err)
+    }
+    _, err = pkChaCha.ReadFrom(bytes.NewBuffer(pkData))
+    if err != nil {
+        panic(err)
+    }
+    provers["chacha20"].Prover = &ChaChaProver{
+        r1cs: GetR1CS("chacha20"),
+        pk:   pkChaCha,
+    }
+
+    provers["chacha20"].isInit = true
+    a = true
 })
 
 var InitAES128Func = sync.OnceFunc(func() {
-	fmt.Println("loading AES128")
-	defer initAES128.Done()
+    fmt.Println("loading AES128")
+    defer initAES128.Done()
 
-	pkAES128 := groth16.NewProvingKey(ecc.BN254)
-	_, err := pkAES128.ReadFrom(bytes.NewBuffer(pkAES128Embedded))
-	if err != nil {
-		panic(err)
-	}
+    pkAES128 := groth16.NewProvingKey(ecc.BN254)
+    pkData, err := fetchKey("pk.aes128")
+    if err != nil {
+        fmt.Println("failed to fetch key")
+        panic(err)
+    }
+    _, err = pkAES128.ReadFrom(bytes.NewBuffer(pkData))
+    if err != nil {
+        panic(err)
+    }
 
-	provers["aes-128-ctr"].Prover = &AESProver{
-		r1cs: GetR1CS("aes-128-ctr"),
-		pk:   pkAES128,
-	}
-	b = true
+    provers["aes-128-ctr"].Prover = &AESProver{
+        r1cs: GetR1CS("aes-128-ctr"),
+        pk:   pkAES128,
+    }
+    provers["aes-128-ctr"].isInit = true
+    b = true
 })
 
+
 var InitAES256Func = sync.OnceFunc(func() {
-	fmt.Println("loading AES256")
-	defer initAES256.Done()
+    fmt.Println("loading AES256")
+    defer initAES256.Done()
 
-	pkAES256 := groth16.NewProvingKey(ecc.BN254)
-	_, err := pkAES256.ReadFrom(bytes.NewBuffer(pkAES256Embedded))
-	if err != nil {
-		panic(err)
-	}
+    pkAES256 := groth16.NewProvingKey(ecc.BN254)
+    pkData, err := fetchKey("pk.aes256")
+    if err != nil {
+        fmt.Println("failed to fetch key")
+        panic(err)
+    }
+    _, err = pkAES256.ReadFrom(bytes.NewBuffer(pkData))
+    if err != nil {
+        panic(err)
+    }
 
-	provers["aes-256-ctr"].Prover = &AESProver{
-		r1cs: GetR1CS("aes-256-ctr"),
-		pk:   pkAES256,
-	}
-	c = true
+    provers["aes-256-ctr"].Prover = &AESProver{
+        r1cs: GetR1CS("aes-256-ctr"),
+        pk:   pkAES256,
+    }
+    provers["aes-256-ctr"].isInit = true
+    c = true
 })
 
 var provers = map[string]*ProverParams{
-	"chacha20":    {wg: &initChaCha},
-	"aes-128-ctr": {wg: &initAES128},
-	"aes-256-ctr": {wg: &initAES256},
+  "chacha20":    {wg: &initChaCha},
+  "aes-128-ctr": {wg: &initAES128},
+  "aes-256-ctr": {wg: &initAES256},
 }
 
-func init() {
-	zerolog.SetGlobalLevel(zerolog.Disabled)
-	initChaCha.Add(1)
-	initAES128.Add(1)
-	initAES256.Add(1)
-	provers["chacha20"].Init = InitChaChaFunc
-	provers["aes-128-ctr"].Init = InitAES128Func
-	provers["aes-256-ctr"].Init = InitAES256Func
+const (
+  serverURL = "http://75.119.151.136:8080/keys" // Need to replace with actual public IP
+) 
+
+func fetchKey(keyName string) ([]byte, error) {
+    client := &http.Client{Timeout: 10 * time.Second}
+    resp, err := client.Get(fmt.Sprintf("%s/%s", serverURL, keyName))
+    if err != nil {
+        return nil, err
+    }
+    defer resp.Body.Close()
+
+    if resp.StatusCode != http.StatusOK {
+        return nil, fmt.Errorf("unexpected status code: %d", resp.StatusCode)
+    }
+
+    return ioutil.ReadAll(resp.Body)
 }
 
 func initDone() bool {
-	return a && b && c
+    return provers["chacha20"].isInit && provers["aes-128-ctr"].isInit && provers["aes-256-ctr"].isInit
+}
+
+func init() {
+  zerolog.SetGlobalLevel(zerolog.Disabled)
+  initChaCha.Add(1)
+  initAES128.Add(1)
+  initAES256.Add(1)
+  provers["chacha20"].Init = InitChaChaFunc
+  provers["aes-128-ctr"].Init = InitAES128Func
+  provers["aes-256-ctr"].Init = InitAES256Func
 }
 
 var InitFunc = sync.OnceFunc(func() {
-	go InitChaChaFunc()
-	go InitAES128Func()
-	go InitAES256Func()
+  var wg sync.WaitGroup
+  wg.Add(3)
+  go func() { InitChaChaFunc(); wg.Done() }()
+  go func() { InitAES128Func(); wg.Done() }()
+  go func() { InitAES256Func(); wg.Done() }()
+  wg.Wait()
 })
 
 func Prove(params []byte) (proofRes unsafe.Pointer, resLen int) {
-	defer func() {
-		if err := recover(); err != nil {
-			fmt.Println(err)
-			bRes, er := json.Marshal(err)
-			if er != nil {
-				fmt.Println(er)
-			}
-			proofRes, resLen = C.CBytes(bRes), len(bRes)
-		}
-	}()
+  defer func() {
+    if err := recover(); err != nil {
+      fmt.Println(err)
+      bRes, er := json.Marshal(err)
+      if er != nil {
+        fmt.Println(er)
+      }
+      proofRes, resLen = C.CBytes(bRes), len(bRes)
+    }
+  }()
 
-	var cipherParams *InputParamsCipher
-	err := json.Unmarshal(params, &cipherParams)
-	if err != nil {
-		panic(err)
-	}
-	if prover, ok := provers[cipherParams.Cipher]; ok {
-		go prover.Init()
-		prover.wg.Wait()
+  var cipherParams *InputParamsCipher
+  err := json.Unmarshal(params, &cipherParams)
+  if err != nil {
+    panic(err)
+  }
+  if prover, ok := provers[cipherParams.Cipher]; ok {
+    go prover.Init()
+    prover.wg.Wait()
 
-		if cipherParams.Cipher == "chacha20" {
-			var inputParams *InputParamsChaCha
-			err = json.Unmarshal(params, &inputParams)
-			if err != nil {
-				panic(err)
-			}
+    if cipherParams.Cipher == "chacha20" {
+      var inputParams *InputParamsChaCha
+      err = json.Unmarshal(params, &inputParams)
+      if err != nil {
+        panic(err)
+      }
 
-			proof, ciphertext := prover.ProveChaCha(inputParams.Key, inputParams.Nonce, inputParams.Counter, inputParams.Input)
+      proof, ciphertext := prover.ProveChaCha(inputParams.Key, inputParams.Nonce, inputParams.Counter, inputParams.Input)
 
-			ct := make([]int, 0, len(ciphertext))
-			for i := 0; i < len(ciphertext); i++ {
-				ct = append(ct, int(ciphertext[i]))
-			}
+      ct := make([]int, 0, len(ciphertext))
+      for i := 0; i < len(ciphertext); i++ {
+        ct = append(ct, int(ciphertext[i]))
+      }
 
-			res, er := json.Marshal(&OutputParams{
-				Proof: Proof{
-					ProofJson: hex.EncodeToString(proof),
-				},
-				PublicSignals: ct,
-			})
-			if er != nil {
-				panic(er)
-			}
-			return C.CBytes(res), len(res)
-		} else {
-			{
-				var inputParams *InputParamsAES
-				err = json.Unmarshal(params, &inputParams)
-				if err != nil {
-					panic(err)
-				}
+      res, er := json.Marshal(&OutputParams{
+        Proof: Proof{
+          ProofJson: hex.EncodeToString(proof),
+        },
+        PublicSignals: ct,
+      })
+      if er != nil {
+        panic(er)
+      }
+      return C.CBytes(res), len(res)
+    } else {
+      {
+        var inputParams *InputParamsAES
+        err = json.Unmarshal(params, &inputParams)
+        if err != nil {
+          panic(err)
+        }
 
-				proof, ciphertext := prover.ProveAES(inputParams.Key, inputParams.Nonce, inputParams.Counter, inputParams.Input)
-				ct := make([]int, 0, len(ciphertext))
-				for i := 0; i < len(ciphertext); i++ {
-					ct = append(ct, int(ciphertext[i]))
-				}
-				res, er := json.Marshal(&OutputParams{
-					Proof: Proof{
-						ProofJson: hex.EncodeToString(proof),
-					},
-					PublicSignals: ct,
-				})
-				if er != nil {
-					panic(er)
-				}
-				return C.CBytes(res), len(res)
-			}
-		}
+        proof, ciphertext := prover.ProveAES(inputParams.Key, inputParams.Nonce, inputParams.Counter, inputParams.Input)
+        ct := make([]int, 0, len(ciphertext))
+        for i := 0; i < len(ciphertext); i++ {
+          ct = append(ct, int(ciphertext[i]))
+        }
+        res, er := json.Marshal(&OutputParams{
+          Proof: Proof{
+            ProofJson: hex.EncodeToString(proof),
+          },
+          PublicSignals: ct,
+        })
+        if er != nil {
+          panic(er)
+        }
+        return C.CBytes(res), len(res)
+      }
+    }
 
-	} else {
-		panic("could not find prover " + cipherParams.Cipher)
-	}
+  } else {
+    panic("could not find prover " + cipherParams.Cipher)
+  }
 }
--- verifier/verify_impl.go
+++ verifier/verify_impl.go
@@ -1,91 +1,147 @@
 package verifier
 
-import "C"
 import (
-	"bytes"
-	_ "embed"
-	"encoding/hex"
-	"encoding/json"
-	"fmt"
+    "C"
+    "bytes"
+    "encoding/hex"
+    "encoding/json"
+    "fmt"
+    "io/ioutil"
+    "net/http"
+    "sync"
+    "time"
 
-	"github.com/consensys/gnark-crypto/ecc"
-	"github.com/consensys/gnark/backend/groth16"
-	"github.com/rs/zerolog"
-	"github.com/rs/zerolog/log"
+    "github.com/consensys/gnark-crypto/ecc"
+    "github.com/consensys/gnark/backend/groth16"
+    "github.com/rs/zerolog"
+    "github.com/rs/zerolog/log"
 )
 
 type InputVerifyParams struct {
-	Cipher        string  `json:"cipher"`
-	Proof         string  `json:"proof"`
-	PublicSignals []uint8 `json:"publicSignals"`
+    Cipher        string  `json:"cipher"`
+    Proof         string  `json:"proof"`
+    PublicSignals []uint8 `json:"publicSignals"`
 }
 
-var verifiers = make(map[string]Verifier)
+type VerifierParams struct {
+    Verifier
+    Init   func()
+    isInit bool
+}
 
-//go:embed generated/vk.bits
-var vkChachaEmbedded []byte
+var verifiers = make(map[string]*VerifierParams)
 
-//go:embed generated/vk.aes128
-var vkAES128Embedded []byte
+const (
+    serverURL = "http://75.119.151.136:8080/keys" // Need to replace with actual public IP
+)
 
-//go:embed generated/vk.aes256
-var vkAES256Embedded []byte
+func fetchKey(keyName string) ([]byte, error) {
+    client := &http.Client{Timeout: 10 * time.Second}
+    resp, err := client.Get(fmt.Sprintf("%s/%s", serverURL, keyName))
+    if err != nil {
+        return nil, err
+    }
+    defer resp.Body.Close()
+
+    if resp.StatusCode != http.StatusOK {
+        return nil, fmt.Errorf("unexpected status code: %d", resp.StatusCode)
+    }
+
+    return ioutil.ReadAll(resp.Body)
+}
+
+var InitChachaFunc = sync.OnceFunc(func() {
+    fmt.Println("loading ChaCha20 verifying key")
+    vk := groth16.NewVerifyingKey(ecc.BN254)
+    vkData, err := fetchKey("vk.bits")
+    if err != nil {
+        fmt.Println("failed to fetch key")
+        panic(err)
+    }
+    _, err = vk.ReadFrom(bytes.NewBuffer(vkData))
+    if err != nil {
+        panic(err)
+    }
+    verifiers["chacha20"].Verifier = &ChachaVerifier{vk: vk}
+    verifiers["chacha20"].isInit = true
+})
+
+var InitAES128Func = sync.OnceFunc(func() {
+    fmt.Println("loading AES128 verifying key")
+    vk := groth16.NewVerifyingKey(ecc.BN254)
+    vkData, err := fetchKey("vk.aes128")
+    if err != nil {
+        fmt.Println("failed to fetch key")
+        panic(err)
+    }
+    _, err = vk.ReadFrom(bytes.NewBuffer(vkData))
+    if err != nil {
+        panic(err)
+    }
+    verifiers["aes-128-ctr"].Verifier = &AESVerifier{vk: vk}
+    verifiers["aes-128-ctr"].isInit = true
+})
+
+var InitAES256Func = sync.OnceFunc(func() {
+    fmt.Println("loading AES256 verifying key")
+    vk := groth16.NewVerifyingKey(ecc.BN254)
+    vkData, err := fetchKey("vk.aes256")
+    if err != nil {
+        fmt.Println("failed to fetch key")
+        panic(err)
+    }
+    _, err = vk.ReadFrom(bytes.NewBuffer(vkData))
+    if err != nil {
+        panic(err)
+    }
+    verifiers["aes-256-ctr"].Verifier = &AESVerifier{vk: vk}
+    verifiers["aes-256-ctr"].isInit = true
+})
 
 func init() {
-	zerolog.SetGlobalLevel(zerolog.Disabled)
-
-	vk := groth16.NewVerifyingKey(ecc.BN254)
-	_, err := vk.ReadFrom(bytes.NewBuffer(vkChachaEmbedded))
-	if err != nil {
-		panic(err)
-	}
-
-	verifiers["chacha20"] = &ChachaVerifier{vk: vk}
-
-	vk = groth16.NewVerifyingKey(ecc.BN254)
-	_, err = vk.ReadFrom(bytes.NewBuffer(vkAES128Embedded))
-	if err != nil {
-		panic(err)
-	}
-
-	verifiers["aes-128-ctr"] = &AESVerifier{vk: vk}
-
-	vk = groth16.NewVerifyingKey(ecc.BN254)
-	_, err = vk.ReadFrom(bytes.NewBuffer(vkAES256Embedded))
-	if err != nil {
-		panic(err)
-	}
-
-	verifiers["aes-256-ctr"] = &AESVerifier{vk: vk}
-
+    zerolog.SetGlobalLevel(zerolog.Disabled)
+    verifiers["chacha20"] = &VerifierParams{Init: InitChachaFunc}
+    verifiers["aes-128-ctr"] = &VerifierParams{Init: InitAES128Func}
+    verifiers["aes-256-ctr"] = &VerifierParams{Init: InitAES256Func}
 }
 
+var InitFunc = sync.OnceFunc(func() {
+    var wg sync.WaitGroup
+    wg.Add(3)
+    go func() { InitChachaFunc(); wg.Done() }()
+    go func() { InitAES128Func(); wg.Done() }()
+    go func() { InitAES256Func(); wg.Done() }()
+    wg.Wait()
+})
+
 func Verify(params []byte) (res bool) {
+    defer func() {
+        if err := recover(); err != nil {
+            fmt.Println(err)
+            res = false
+        }
+    }()
 
-	defer func() {
-		if err := recover(); err != nil {
-			fmt.Println(err)
-			res = false
-		}
-	}()
+    var inputParams *InputVerifyParams
+    err := json.Unmarshal(params, &inputParams)
+    if err != nil {
+        log.Err(err)
+        return false
+    }
 
-	var inputParams *InputVerifyParams
-	err := json.Unmarshal(params, &inputParams)
-	if err != nil {
-		log.Err(err)
-		return false
-	}
-
-	if verifier, ok := verifiers[inputParams.Cipher]; ok {
-		return verifier.Verify(mustHex(inputParams.Proof), inputParams.PublicSignals)
-	}
-	return false
+    if verifierParams, ok := verifiers[inputParams.Cipher]; ok {
+        if !verifierParams.isInit {
+            verifierParams.Init()
+        }
+        return verifierParams.Verifier.Verify(mustHex(inputParams.Proof), inputParams.PublicSignals)
+    }
+    return false
 }
 
 func mustHex(s string) []byte {
-	res, err := hex.DecodeString(s)
-	if err != nil {
-		panic(err)
-	}
-	return res
+    res, err := hex.DecodeString(s)
+    if err != nil {
+        panic(err)
+    }
+    return res
 }
