a86ed3bff5bbbeb74179fbfe91277233c8d4f15a
Alex E <alex@creatoros.co>
2024-10-16 17:18:44 +0500

    [FEAT] working DLEQ for 1st case

--- utils_test/nullification_test.go
+++ utils_test/nullification_test.go
@@ -70,29 +70,38 @@ type testData struct {
 func prepareTestData(assert *test.Assert) testData {
 	curve := tbn254.GetEdwardsCurve()
 	secretData := (&big.Int{}).SetBytes([]byte("alex@reclaimprotocol.org"))
+
+	// random mask
 	r, _ := rand.Int(rand.Reader, scalarField)
 
-	serverPrivate, _ := rand.Int(rand.Reader, scalarField)
+	sk, _ := rand.Int(rand.Reader, scalarField)
 	serverPublic := &tbn254.PointAffine{}
-	serverPublic.ScalarMultiplication(&curve.Base, serverPrivate)
+	serverPublic.ScalarMultiplication(&curve.Base, sk) // G*sk
 
-	dataPoint := hashToCurve(secretData.Bytes())
-	assert.True(dataPoint.IsOnCurve())
+	H := hashToCurve(secretData.Bytes()) // H
+	assert.True(H.IsOnCurve())
 
 	// mask
 	masked := &tbn254.PointAffine{}
-	masked.ScalarMultiplication(dataPoint, r)
+	masked.ScalarMultiplication(H, r) // H*r
 
 	// server part
 	resp := &tbn254.PointAffine{}
-	resp.ScalarMultiplication(masked, serverPrivate)
+	resp.ScalarMultiplication(masked, sk) // H*r*sk
 
 	// nullifier calc
 	invR := new(big.Int)
-	invR.ModInverse(r, scalarField)
+	invR.ModInverse(r, scalarField) // r^-1
 
 	nullifier := &tbn254.PointAffine{}
-	nullifier.ScalarMultiplication(resp, invR)
+	nullifier.ScalarMultiplication(resp, invR) // H *r * sk * r^-1 = H * sk
+
+	testNullifier := &tbn254.PointAffine{}
+	testNullifier.ScalarMultiplication(H, sk)
+
+	assert.True(testNullifier.Equal(nullifier))
+
+	ProveDLEQ(assert, sk, serverPublic, resp, masked)
 
 	return testData{
 		response:   OutPointToInPoint(resp),
@@ -114,7 +123,15 @@ func OutPointToInPoint(point *tbn254.PointAffine) twistededwards.Point {
 func hashBN(data ...[]byte) []byte {
 	hasher := hash.MIMC_BN254.New()
 	for _, d := range data {
-		hasher.Write(d)
+		_, err := hasher.Write(d)
+		if err != nil {
+			r := new(big.Int).SetBytes(d)
+			r.Mod(r, scalarField)
+			_, err = hasher.Write(r.Bytes())
+			if err != nil {
+				panic(err)
+			}
+		}
 	}
 	return hasher.Sum(nil)
 }
@@ -139,6 +156,124 @@ func TestMaskUnmask(t *testing.T) {
 	deblinded.ScalarMultiplication(blinded, invR)
 	fmt.Println(r, invR)
 	assert.True(deblinded.Equal(data))
+}
+
+func ProveDLEQ(assert *test.Assert, x *big.Int, xG, xH, H *tbn254.PointAffine) {
+
+	// xG = G*x xH = H*x
+
+	curve := tbn254.GetEdwardsCurve()
+	base := curve.Base
+	// random scalar
+	v, err := rand.Int(rand.Reader, scalarField)
+	assert.NoError(err)
+
+	vg := new(tbn254.PointAffine)
+	vg.ScalarMultiplication(&base, v) // G*v
+
+	vh := new(tbn254.PointAffine)
+	vh.ScalarMultiplication(H, v) // H*v
+
+	challengeHash := hashBN(xG.Marshal(), xH.Marshal(), vg.Marshal(), vh.Marshal()) // TODO
+	c := new(big.Int).SetBytes(challengeHash)
+	c.Mod(c, scalarField) // ?
+
+	r := new(big.Int).Neg(c) // r = -c
+	r.Mul(r, x)              // r = -c*x
+	r.Add(r, v)              // r = v - c*x
+	r.Mod(r, scalarField)
+
+	/*
+		vG==rG+c(xG)
+		vH==rH+c(xH)
+	*/
+	rg := new(tbn254.PointAffine).ScalarMultiplication(&base, r) // G * r = G * (v-c*x)
+	chg := new(tbn254.PointAffine).ScalarMultiplication(xG, c)   // G*x*c
+
+	rg.Add(rg, chg) // G * (v-c*x) + G*x*c =G*v − G*c*x + G*c*x = vG
+	assert.True(rg.Equal(vg))
+
+}
+
+func TestArithBN(t *testing.T) {
+	assert := test.NewAssert(t)
+
+	curveOrder := ecc.BN254.ScalarField()
+
+	// random element
+	x, _ := rand.Int(rand.Reader, curveOrder)
+
+	// random element
+	y, _ := rand.Int(rand.Reader, curveOrder)
+
+	t1 := &bn254.G1Affine{}
+	t1.ScalarMultiplicationBase(x) // G*x
+
+	t2 := &bn254.G1Affine{}
+	t2.ScalarMultiplicationBase(y) // G*y
+
+	t3 := &bn254.G1Affine{}
+	t3.Add(t1, t2) // G*x + G*y
+
+	z := new(big.Int)
+	z.Add(x, y)
+	z.Mod(z, curveOrder)
+
+	t4 := &bn254.G1Affine{}
+	t4.ScalarMultiplicationBase(z)
+	assert.True(t4.Equal(t3))
+
+	w := new(big.Int)
+	w.Neg(y)
+	w.Mod(w, curveOrder)
+	t5 := &bn254.G1Affine{}
+	t5.ScalarMultiplicationBase(w) // G* -y
+
+	t6 := &bn254.G1Affine{}
+	t6.Add(t3, t5) // G* -y + G*x + G*y = G*x
+
+	assert.True(t6.Equal(t1))
+}
+
+func TestArithBabyJub(t *testing.T) {
+	assert := test.NewAssert(t)
+
+	curveOrder := tbn254.GetEdwardsCurve().Order
+	base := tbn254.GetEdwardsCurve().Base
+
+	// random element
+	x, _ := rand.Int(rand.Reader, &curveOrder)
+
+	// random element
+	y, _ := rand.Int(rand.Reader, &curveOrder)
+
+	t1 := &tbn254.PointAffine{}
+	t1.ScalarMultiplication(&base, x) // G*x
+
+	t2 := &tbn254.PointAffine{}
+	t2.ScalarMultiplication(&base, y) // G*y
+
+	t3 := &tbn254.PointAffine{}
+	t3.Add(t1, t2) // G*x + G*y
+
+	z := new(big.Int)
+	z.Add(x, y) // x+y
+	z.Mod(z, &curveOrder)
+
+	t4 := &tbn254.PointAffine{}
+	t4.ScalarMultiplication(&base, z) // G * (x+y)
+	assert.True(t4.Equal(t3))
+
+	w := new(big.Int)
+	w.Neg(y) // -y
+	w.Mod(w, &curveOrder)
+	t5 := &tbn254.PointAffine{}
+	t5.ScalarMultiplication(&base, w) // G* -y
+
+	t6 := &tbn254.PointAffine{}
+	t6.Add(t3, t5) // G* -y + G*x + G*y = G*x
+
+	assert.True(t6.Equal(t1))
 }
 
 func proveSuccess(assert *test.Assert, sk *fr.Element, pk *bn254.G1Affine, resp *bn254.G1Affine) (term *bn254.G1Affine, res *fr.Element) {
@@ -208,11 +343,11 @@ func VerifyProof(assert *test.Assert, pk *bn254.G1Affine, term *bn254.G1Affine, 
 	err = tpk.Unmarshal(pk.Marshal()) // G*sk
 	assert.NoError(err)
 
-	t1Point := tpk.ScalarMultiplication(tpk, challenge.BigInt(&big.Int{}))
-	t1Point.Add(t1Point, term)
+	t1Point := tpk.ScalarMultiplication(tpk, challenge.BigInt(&big.Int{})) // G*sk * challenge
+	t1Point.Add(t1Point, term)                                             // G*sk * challenge + G*x
 
 	t2Point := &bn254.G1Affine{}
-	t2Point.ScalarMultiplicationBase(res.BigInt(&big.Int{}))
+	t2Point.ScalarMultiplicationBase(res.BigInt(&big.Int{})) // G*res = G (sk * challenge + x)
 	assert.True(t1Point.Equal(t2Point))
 
 	/*t1 = term3.Add(c.serverPublicKey.ScalarMultInt(challenge))
